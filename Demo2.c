bdda6d8062e58226163ba501a029176b7a790035
Aman <amantiwari8861@gmail.com>
2021-10-01 10:22:24 +0530

    dome done

---
+++ Demo2.c
@@ -0,0 +1,13 @@
+#include<stdio.h> // pre processor directives or header file
+int main() //main function or starting point of program
+{   //body open of main function
+    printf("hello my name is aman ");// to print msg on console
+    return 0;//ending point of a program
+}   //body close of main function
+//  ; -> end of line (statement)
+// single line comment
+/*
+    multiple
+    line
+    comment
+*/
